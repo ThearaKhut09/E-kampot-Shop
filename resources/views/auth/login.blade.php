6780609528c3f2908130f5b7a2f825947e2aef38
ThearaKhut09 <khut.theara.2822@rupp.edu.kh>
2025-07-05 09:45:32 +0700

    fix-form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ __('Log in') }} - {{ config('app.name', 'E-Kampot Shop') }}</title>
+        <title>{{ __('Log in') }} - E-Kampot Shop</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ __('Reset Password') }} - {{ config('app.name', 'E-Kampot Shop') }}</title>
+        <title>{{ __('Reset Password') }} - E-Kampot Shop</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
